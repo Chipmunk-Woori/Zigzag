330821f71ece0318229f2665db2e0827f9b9c2b4
Chipmunk-Woori <haha51015@naver.com>
2022-01-04 22:30:20 +0900

    하단탭 '마이페이지' _'포인트' : ①화면 디자인 작업(완성) ②버튼 기능(완성)

--- Screens/MyPage/Point.js
+++ Screens/MyPage/Point.js
@@ -16,9 +16,187 @@ import {
 const screenWidth = Dimensions.get("screen").width; // 전체화면 가로길이
 const screenHeight = Dimensions.get("screen").height; //전체화면 세로길이
 
+const PointData = [
+  {
+    type: "use",
+    date: "21.12.17",
+    price: 1000,
+    title: "야간 깜짝 포인트",
+    deadline: "21.12.18",
+  },
+  {
+    type: "save",
+    date: "21.11.22",
+    price: 100,
+    title: "블랙쇼핑지원금_100",
+    deadline: "21.12.01",
+  },
+  {
+    type: "exp",
+    date: "21.08.08",
+    price: 98,
+    title: "구매확정 적립",
+    deadline: "22.08.08",
+  },
+  {
+    type: "save",
+    date: "21.08.06",
+    price: 94,
+    title: "구매확정 적립",
+    deadline: "22.08.06",
+  },
+  {
+    type: "save",
+    date: "21.08.03",
+    price: 1000,
+    title: "쿠폰해지 보상 적립",
+    deadline: "21.08.10",
+  },
+  {
+    type: "use",
+    date: "21.05.28",
+    price: 325,
+    title: "구매확정 적립",
+    deadline: "22.05.28",
+  },
+  {
+    type: "use",
+    date: "21.05.28",
+    price: 325,
+    title: "구매확정 적립",
+    deadline: "22.05.28",
+  },
+  {
+    type: "exp",
+    date: "21.08.06",
+    price: 94,
+    title: "구매확정 적립",
+    deadline: "22.08.06",
+  },
+  {
+    type: "exp",
+    date: "21.08.03",
+    price: 1000,
+    title: "쿠폰해지 보상 적립",
+    deadline: "21.08.10",
+  },
+];
+
 const Point = ({ navigation }) => {
   const [retainedPoint, setRetainedPoint] = useState("192");
-  const [button, setButton] = useState(["전체", "적립", "사용", "소멸"]);
+  const [buttonTitle, setButtonTitle] = useState([
+    "전체",
+    "적립",
+    "사용",
+    "소멸",
+  ]);
+  const [choicedButtonTitle, setChoicedButtonTitle] = useState("전체");
+  const [choicedData, setChoicedData] = useState([]);
+
+  useEffect(() => {
+    let temp = [...PointData];
+    setChoicedData(temp);
+  }, []);
+
+  const returnIconImage = item => {
+    if (item !== "") {
+      switch (item.type) {
+        case "save":
+          return (
+            <Image
+              source={require("../../assets/icon/reservedPoint.png")}
+              style={styles.pointIcon}
+            />
+          );
+        case "use":
+          return (
+            <Image
+              source={require("../../assets/icon/usedPoint.png")}
+              style={styles.pointIcon}
+            />
+          );
+        case "exp":
+          return (
+            <Image
+              source={require("../../assets/icon/lapsedPoint.png")}
+              style={styles.pointIcon}
+            />
+          );
+      }
+    }
+  };
+
+  const returnContent = item => {
+    if (item !== "") {
+      switch (item.type) {
+        case "save":
+          return (
+            <View>
+              <Text style={styles.pointPrice}>+{item.price}원</Text>
+              <Text style={styles.pointTitle}>{item.title}</Text>
+              <Text style={styles.pointDeadline}>{item.deadline}까지</Text>
+            </View>
+          );
+        case "use":
+          return (
+            <View>
+              <Text style={[styles.pointPrice, styles.usedPointColor]}>
+                +{item.price}원
+              </Text>
+              <Text style={styles.pointTitle}>결제 시 사용</Text>
+            </View>
+          );
+        case "exp":
+          return (
+            <View>
+              <Text style={[styles.pointPrice, styles.lapsedPointColor]}>
+                +{item.price}원
+              </Text>
+              <Text style={styles.pointTitle}>소멸</Text>
+            </View>
+          );
+      }
+    }
+  };
+
+  const choicedButton = i => {
+    let tempArray = [...PointData];
+
+    if (i !== "") {
+      switch (i) {
+        case "적립":
+          tempArray = tempArray.filter(fi => {
+            return fi.type == "save";
+          });
+          break;
+        case "사용":
+          tempArray = tempArray.filter(fi => {
+            return fi.type == "use";
+          });
+          break;
+        case "소멸":
+          tempArray = tempArray.filter(fi => {
+            return fi.type == "exp";
+          });
+          break;
+      }
+    }
+
+    setChoicedButtonTitle(i);
+    setChoicedData(tempArray);
+  };
+
+  returnButtonState = i => {
+    let state = false;
+    if (i !== "") {
+      if (choicedButtonTitle == i) {
+        state = true;
+      }
+
+      return state;
+    }
+  };
+
   return (
     <View style={styles.ViewStyle}>
       <View style={styles.headerView}>
@@ -46,20 +224,60 @@ const Point = ({ navigation }) => {
         </View>
       </View>
 
-      <View style={{ flexDirection: "row" }}>
-        {button.map(i => {
+      <View style={{ flexDirection: "row", marginBottom: 19 }}>
+        {buttonTitle.map(i => {
           return (
             <View style={styles.buttonView}>
-              <TouchableOpacity style={styles.buttonTouch}>
-                <Text style={styles.buttonText}>{i}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  choicedButton(i);
+                }}
+                style={
+                  returnButtonState(i)
+                    ? [styles.buttonTouch, styles.choicedButton]
+                    : styles.buttonTouch
+                }
+              >
+                <Text
+                  style={
+                    returnButtonState(i)
+                      ? [styles.buttonText, styles.choicedButtonText]
+                      : styles.buttonText
+                  }
+                >
+                  {i}
+                </Text>
               </TouchableOpacity>
             </View>
           );
         })}
       </View>
-      <View>
-        <FlatList />
-      </View>
+
+      <TouchableOpacity style={styles.questionTouch}>
+        <Text style={styles.questionText}>
+          포인트 적립, 사용방법이 궁금해요 👀
+        </Text>
+        <Image
+          source={require("../../assets/icon/next.png")}
+          style={{ width: 15, height: 15 }}
+        />
+      </TouchableOpacity>
+
+      <FlatList
+        data={choicedData}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.pointView}>
+              <Text style={styles.pointDate}>{item.date}</Text>
+              <View style={styles.pointContentView}>
+                {returnIconImage(item)}
+                <View style={styles.pointTextView}>{returnContent(item)}</View>
+              </View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
@@ -136,6 +354,72 @@ const styles = StyleSheet.create({
   choicedButton: {
     backgroundColor: "black",
     borderColor: "black",
+  },
+  questionTouch: {
+    flexDirection: "row",
+    width: "100%",
+    height: 38,
+    borderColor: "lightgray",
+    borderStyle: "solid",
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 14,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  questionText: {
+    fontSize: 12,
+    color: "gray",
+  },
+  pointView: {
+    width: "100%",
+    height: 118,
+    borderColor: "lightgray",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 10,
+    padding: 13,
+  },
+  pointContentView: {
+    flexDirection: "row",
+    height: 80,
+    alignItems: "center",
+  },
+  pointTextView: {
+    height: 50,
+  },
+  pointIcon: {
+    width: 55,
+    height: 55,
+    marginRight: 16,
+  },
+  pointDate: {
+    fontSize: 13,
+    fontWeight: "500",
+    marginBottom: 7,
+  },
+  pointPrice: {
+    color: "#F719A3",
+    fontWeight: "500",
+    fontSize: 15,
+    marginBottom: 2,
+  },
+  usedPointColor: {
+    color: "#5153FF",
+  },
+  lapsedPointColor: {
+    color: "gray",
+  },
+  pointTitle: {
+    fontSize: 13,
+    fontWeight: "500",
+    marginBottom: 3,
+  },
+  pointDeadline: {
+    fontWeight: "500",
+    color: "gray",
+    fontSize: 11,
   },
 });
 export default Point;
